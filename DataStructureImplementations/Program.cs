eba76d050369a298a85db5cf539a678d7cb1f0a6
Leonel Antonio Santiago Baez <santiagobaez@msn.com>
2018-05-07 02:13:28 -0400

    Basic implementation of a LinkedList<T>.
    Using a code sample and modernizing it from: https://www.codeproject.com/Articles/27742/How-To-Reverse-a-Linked-List-Different-Ways

--- DataStructureImplementations/Program.cs
+++ DataStructureImplementations/Program.cs
@@ -10,6 +10,54 @@ namespace DataStructureImplementations
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Add First:");
+            var myList1 = new LinkedList<int>();
+
+            myList1.AddFirst(4);
+            myList1.AddFirst(10);
+            myList1.AddFirst(25);
+
+            ReverseLinkedList(myList1);
+
+            Console.ReadLine();
+        }
+
+        public static void ReverseLinkedList(LinkedList<int> linkedList)
+        {
+            // ------------------------------------------------------------
+            // Create a new linked list and add all items of given 
+            // linked list to the copy linked list in reverse order
+            // ------------------------------------------------------------
+
+            LinkedList<int> copyList = new LinkedList<int>();
+
+            // ------------------------------------------------------------
+            // Start from the latest node
+            // ------------------------------------------------------------
+
+            LinkedListNode<int> start = linkedList.First;
+
+            // ------------------------------------------------------------
+            // Traverse until the first node is found
+            // ------------------------------------------------------------
+
+            while (start != null)
+            {
+                // ------------------------------------------------------------
+                // Add item to the new link list
+                // ------------------------------------------------------------
+
+                copyList.AddFirst(start.Value);
+
+                start = start.Previous;
+                Console.WriteLine(copyList.First);
+            }
+
+            //linkedList = copyList;
+
+            // ------------------------------------------------------------
+            // That's it!
+            // ------------------------------------------------------------
         }
     }
 }
